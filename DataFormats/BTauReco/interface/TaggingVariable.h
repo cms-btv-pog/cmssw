55e62103f39e5d8cc06865b1cf3d050d2254c28c
sarmad <smshaheen@ihep.ac.cn>
2017-08-23 18:37:43 +0200

    Fixed the issues for the compilation of the new variables.

--- DataFormats/BTauReco/interface/TaggingVariable.h
+++ DataFormats/BTauReco/interface/TaggingVariable.h
@@ -40,7 +40,8 @@ namespace reco {
       jetNTracks,                               // tracks associated to jet
       jetNSelectedTracks,                       // tracks associated to jet
       jetNTracksEtaRel,                         // number of tracks for which etaRel is computed
-
+      
+      jetNchTracks,                             // total charged and good tracks associated  to jet
       jetNTracks_PV,                            // tracks associated to jet coming from primary Vertex
       jetNTracks_nonPV,                         // tracks associated to jet not coming from primary Vertex
       jetPt_Tracks,                             // jet transverse momentum from the vector sum of the transverse momentum of associated tracks
